0f9bbe291a1b31fc402d240c1509c6a12fc0f197
AlejoDEV <danijandro03@outlook.com>
2021-07-15 15:29:08 -0500

    Se crean y registran Pages a Editar

--- WebcatApp/ViewModel/Base/ViewModelLocator.cs
+++ WebcatApp/ViewModel/Base/ViewModelLocator.cs
@@ -24,11 +24,19 @@ namespace WebcatApp.ViewModel.Base
                 SimpleIoc.Default.Register<INavigationService, NavigationService>();
                 SimpleIoc.Default.Register<InitPageModel>();
                 nav.Configure("InitPage", typeof(InitPage));
+                SimpleIoc.Default.Register<TreePageModel>();
+                nav.Configure("TreePage", typeof(TreePage));
+                SimpleIoc.Default.Register<PdfPageModel>();
+                nav.Configure("PdfPage", typeof(PdfPage));
+                SimpleIoc.Default.Register<UxPageModel>();
+                nav.Configure("UxPage", typeof(UxPage));
 
             }
         }
         public InitPageModel InitPage => ServiceLocator.Current.GetInstance<InitPageModel>();
-        
-        
+        public TreePageModel TreePage => ServiceLocator.Current.GetInstance<TreePageModel>();
+        public PdfPageModel PdfPage => ServiceLocator.Current.GetInstance<PdfPageModel>();
+        public UxPageModel UxPage => ServiceLocator.Current.GetInstance<UxPageModel>();
+
     }
 }
